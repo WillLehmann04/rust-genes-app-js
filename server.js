1e22fa7f7bcb5ad62f5adda42fff8c250798368c
Will Lehmann <will.lehmann4@gmail.com>
2023-07-22 20:30:26 +0930

    Updated general information
    Updated several files with base to get started with configurations

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const app = require("express")();
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs')
+
+app.use('/favicon.ico', express.static('images/border.png'));
 
 app.get('/', (request, response) => {
     console.log("yo");
-    response.send("hi")
+    response.render("pages/home", {title: "Rust Genetics - Calculator"})
 })
 
 app.listen(8989)
