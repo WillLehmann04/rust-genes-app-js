b57ed9365f173d7e309822745c5292db19c16c09
Will Lehmann <will.lehmann4@gmail.com>
2023-07-23 14:59:13 +0930

    Setup Cont.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
+const cookieParser = require('cookie-parser')
 var app = express();
 
 app.set('view engine', 'ejs')
 
 app.use('/favicon.ico', express.static('images/border.png'));
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}))
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    console.log("yo");
-    response.render("pages/home", {title: "Rust Genetics - Calculator"})
+    response.render("pages/home", {title: "Rust Genetics - Calculator", consentStatus:request.cookies['cookie-consent']})
 })
+
+const cookieRoute = require("./routes/cookies");
+app.use("/cookies", cookieRoute)
 
 app.listen(8989)
